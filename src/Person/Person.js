0fee8b85bb8648b90fcdf4becc51d038de55f189
khenryjoe <joseph.ketterer@gmail.com>
2019-02-20 12:36:00 +0000

    first commit react course

---
+++ src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+           
+    <p onClick={props.click}>I'm {props.name} and I am {props.age}</p>
+    <p>{props.children}</p>
+    <input type="text" />
+    </div>
+    )
+}
+
+export default person;
